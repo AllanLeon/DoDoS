e43da3a5960706da535c61f3759c2b7a0ee499e8
Franco Montiel <francomontiel@hotmail.com>
2016-11-06 01:49:13 -0400

    Attacker script now sends sensor data as JSON

--- attacker.js
+++ attacker.js
@@ -1,10 +1,44 @@
 var express = require("express");
 var app = express();
+var request = require("request");
 app.use(express.static(__dirname + '/'));
 
-app.post("/attacker", function(req, res) {
-	req.write("Hi, BITCH!");
-});
+function randomizer(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function buildJSONbody() {
+	var sensor0, sensor1, sensor2;
+	var d = new Date();
+
+	sensor0 = randomizer(-20, 20) * 5;
+	sensor1 = randomizer(-20, 20) * 5;
+	sensor2 = randomizer(-20, 20) * 5;
+
+	var jsonbody = "{ \"id\" : \"My IoT\", \"datetime\" : \"";
+	jsonbody += d.getFullYear() + "-";
+	jsonbody += (d.getMonth()+1) + "-";
+	jsonbody += d.getDate() + " ";
+	jsonbody += d.getHours() + ":";
+	jsonbody += d.getMinutes() + ":";
+	jsonbody += d.getSeconds();
+	jsonbody += "\", \"data\" : { \"sensor0\" : " + sensor0;
+	jsonbody += ", \"sensor1\" : " + sensor1;
+	jsonbody += ", \"sensor2\" : " + sensor2 + " }}";
+
+	return jsonbody;
+}
+
+function callPost() {
+	request.post({
+	  headers: {"content-type" : "application/json"},
+	  url:     "http://localhost:8080/attacker",
+	  body:    buildJSONbody()
+	}, function(error, res, body){
+	  console.log(body);
+	});	
+}
+
 
 //Starts this attacker. Default Port would be 8081, else +1
 var port = 8081;
@@ -19,3 +53,4 @@ function connect(p) {
 }
 
 connect(port);
+setInterval(callPost, 1000);
