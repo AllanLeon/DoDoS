4a338adcb427a13df18917627fd1034eb950a15c
Allan Leon <aleon.107@outlook.com>
2016-11-08 18:15:20 -0400

    Created victim

---
+++ victim.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.use(express.static(__dirname + '/'));
+
+app.post("/", function(req, res) {
+	console.log("HELP!");
+});
+
+app.get("/", function(req, res) {
+	console.log("HELP!");
+});
+
+//Starts this victim, default port is 3000
+var port = 3000;
+app.listen(port);
+console.log("Victim running on: " + port);
